a2d1b47c74e7973fb5ba2a24be619bdf9c2fbae4
Michael Schout <mschout@gkg.net>
2009-04-30 16:34:22 -0500

    use config file and syslog

--- lib/Milter/Limit.pm
+++ lib/Milter/Limit.pm
@@ -2,46 +2,80 @@ package Milter::Limit;
 
 use strict;
 use base qw(Class::Accessor Class::Singleton);
+use Milter::Limit::Config;
 use Sendmail::PMilter ':all';
+use Sys::Syslog ();
 use Carp;
 
 __PACKAGE__->mk_accessors(qw(driver milter));
 
 sub _new_instance {
     my ($class, $driver) = @_;
-    warn "_new_instance($driver)\n";
 
     croak "usage: new(driver)" unless defined $driver;
+
+    my $self = $class->SUPER::_new_instance();
+
+    $self->init($driver);
+    return $self;
+}
+
+sub init {
+    my ($self, $driver) = @_;
+
+    $self->_init_log;
+
+    $self->milter(new Sendmail::PMilter);
+
+    $self->_init_driver($driver);
+}
+
+sub _init_log {
+    my $self = shift;
+
+    my $conf = $self->config->section('log');
+    $$conf{identity} ||= 'milter-limit';
+    $$conf{facility} ||= 'mail';
+
+    Sys::Syslog::openlog($$conf{identity}, $$conf{options}, $$conf{facility});
+
+    $SIG{__WARN__} = sub {
+        Sys::Syslog::syslog('warning', "warning: ".join('', @_));
+    };
+
+    $SIG{__DIE__}  = sub {
+        Sys::Syslog::syslog('crit', "fatal: ".join('',@_));
+        die @_;
+    };
+}
+
+sub _init_driver {
+    my ($self, $driver) = @_;
 
     my $driver_class = "Milter::Limit::Plugin::$driver";
 
     eval "require $driver_class";
+    if ($@) {
+        die "failed to load $driver_class: $@\n";
+    }
 
-    my $driver_instance = $driver_class->instance();
-
-    my $self = $class->SUPER::_new_instance();
-
-    $self->driver($driver_instance);
-    $self->milter(new Sendmail::PMilter);
-
-    warn "$self driver: ", $self->driver, "\n";
-    warn "$self milter: ", $self->milter, "\n";
-
-    return $self;
+    $self->driver($driver_class->instance);
 }
+
 
 sub register {
     my $self = shift;
 
     my $milter = $self->milter;
 
-    $milter->auto_setconn('milter-limit') or croak "auto_setconn failed";
+    $milter->auto_setconn($self->config->global->{name})
+        or croak "auto_setconn failed";
 
     my %callbacks = (
         envfrom => \&_envfrom_callback
     );
 
-    $milter->register('milter-limit', \%callbacks, SMFI_CURR_ACTS);
+    $milter->register($self->config->global->{name}, \%callbacks, SMFI_CURR_ACTS);
 }
 
 sub main {
@@ -49,9 +83,11 @@ sub main {
 
     my $milter = $self->milter;
 
+    my $conf = $self->config->global;
+
     my $dispatcher = Sendmail::PMilter::prefork_dispatcher(
-        max_children           => 5,
-        max_requests_per_child => 100);
+        max_children           => $$conf{max_children} || 5,
+        max_requests_per_child => $$conf{max_requests_per_child} || 100);
 
     $milter->set_dispatcher($dispatcher);
     $milter->main;
@@ -72,6 +108,11 @@ sub _envfrom_callback {
     else {
         return SMFIS_CONTINUE;
     }
+}
+
+# shortcut to get the config.
+sub config {
+    Milter::Limit::Config->instance;
 }
 
 1;
